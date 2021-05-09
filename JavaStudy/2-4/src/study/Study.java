96a593be60e37e10f89cfa55ff03a88b0680bbd6
domino0084 <77517829+domino0084@users.noreply.github.com>
2021-05-09 19:35:16 +0900

    第2章_パッケージ_課題提出

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -18,6 +18,7 @@ public class Study {
     	 *     また、課題の画像と同じ表示になるように
     	 *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
     	 */
-    	fruits.Fruits.printFruits( FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+    	//fruits.Fruits.printFruits( FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+    	Fruits.printFruits( FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
     }
 }
